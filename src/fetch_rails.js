8299e52a11ff55a715fb49b7616fe70647ae61bf
Miguel Savignano <migue.masx@gmail.com>
2017-04-10 03:03:20 +0200

    fetch rails

--- src/fetch_rails.js
+++ src/fetch_rails.js
@@ -28,16 +28,6 @@ const parseJSON = response => response.json()
 
 const parseText = response => response.text()
 
-const checkStatus = (response) => {
-  console.log("no pases por aqui cabron")
-  if (response.status >= 200 && response.status < 300) {
-    return response
-  } else {
-    var error = new Error(response.statusText)
-    error.response = response
-    throw error
-  }
-}
 const Fetch = {
   checkStatus: (response) => {
     return new Promise( (resolve, reject) => {
